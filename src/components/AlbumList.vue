11385e4163632c7260936d8f236d873a3da50d8d
Difelice-e <difelice.emanuele@gmail.com>
2022-03-14 16:42:28 +0100

    trasformata select in componente + recuperato filtro con emit

--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,24 +1,6 @@
 <template>
   <section>
-      <div class="container filter-select">
-          <label for="select-genre">Generi Musicali:</label>
-          <select name="genre" id="select-genre" v-model="genreFilter">
-              <option value="">Tutti</option>
-              <option 
-              v-for="(genre, i) in genreList" 
-              :key="i" 
-              :value="genre">{{ genre }}</option>
-          </select>
-          
-          <label for="select-artist">Artista:</label>
-          <select name="artist" id="select-artist" v-model="artistFilter">
-              <option value="">Tutti</option>
-              <option 
-              v-for="(artist, i) in artistList" 
-              :key="i" 
-              :value="artist">{{ artist }}</option>
-          </select>
-      </div>
+      <AlbumSelect @cerca="setGenreToFIlter" :genreList="genreList" :artistList="artistList"/>
 
       <div class="container album-wrapper">
           <AlbumItem 
@@ -33,10 +15,12 @@
 <script>
 import axios from 'axios'
 import AlbumItem from './AlbumItem.vue'
+import AlbumSelect from './AlbumSelect.vue'
 
 export default {
     components: {
-        AlbumItem
+        AlbumItem,
+        AlbumSelect
     },
     data() {
         return {
@@ -75,6 +59,9 @@ export default {
                 }
             });
         },
+        setGenreToFIlter: function (filter) {
+            this.genreFilter = filter
+        }
     },
     created() {
         this.fetchAlbum()
@@ -98,22 +85,7 @@ section {
     width: 100%;
 }
 
-.filter-select {
-    text-align: center;
-    margin-bottom: 30px;
 
-    label {
-        color: white;
-        margin-right: 5px;
-    }
-
-    select {
-        width: 150px;
-        background-color: transparent;
-        color: var(--app-text);
-        margin-right: 20px;
-    }
-}
 
 .album-wrapper {
     display: flex;
---
+++ src/components/AlbumSelect.vue
@@ -0,0 +1,61 @@
+<template>
+  <section>
+      <div class="container filter-select">
+          <label for="select-genre">Generi Musicali:</label>
+          <select @change="filtraAlbum" name="genre" id="select-genre" v-model="genreFilter">
+              <option value="">Tutti</option>
+              <option 
+              v-for="(genre, i) in genreList" 
+              :key="i" 
+              :value="genre">{{ genre }}</option>
+          </select>
+          
+          <!-- <label for="select-artist">Artista:</label>
+          <select name="artist" id="select-artist" v-model="artistFilter">
+              <option value="">Tutti</option>
+              <option 
+              v-for="(artist, i) in artistList" 
+              :key="i" 
+              :value="artist">{{ artist }}</option>
+          </select> -->
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    props: {
+        genreList: Array,
+        artistList: Array,
+    },
+    data() {
+        return {
+        genreFilter: ''
+        }
+    },
+    methods: {
+    filtraAlbum: function() {
+        this.$emit('cerca', this.genreFilter)
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.filter-select {
+    text-align: center;
+    margin-bottom: 30px;
+
+    label {
+        color: white;
+        margin-right: 5px;
+    }
+
+    select {
+        width: 150px;
+        background-color: transparent;
+        color: var(--app-text);
+        margin-right: 20px;
+    }
+}
+</style>
